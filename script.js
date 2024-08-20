2a850cd9b90679ae42468600b3b41709174397a8
Dikshantw <dikshantw11@gmail.com>
2024-08-20 20:23:50 +0530

    animation improvement

--- script.js
+++ script.js
@@ -18,8 +18,18 @@ function handleSubmit() {
     const lift = document.createElement("div");
     lift.classList.add("lift");
     lift.setAttribute("isMoving", "false");
+
+    const doors = document.createElement("div");
+    doors.classList.add("doors");
+    const leftDoor = document.createElement("div");
+    leftDoor.classList.add("leftDoor");
+    const rightDoor = document.createElement("div");
+    rightDoor.classList.add("rightDoor");
+    doors.appendChild(leftDoor);
+    doors.appendChild(rightDoor);
     const liftLabel = document.createElement("span");
     liftLabel.textContent = `Lift ${i}`;
+    lift.appendChild(doors);
     lift.appendChild(liftLabel);
     liftContainer.appendChild(lift);
   }
@@ -28,11 +38,17 @@ function handleSubmit() {
     const floor = document.createElement("div");
     const floorcontrollers = document.createElement("div");
     const upBtn = document.createElement("button");
+    upBtn.classList.add("upBtn");
     const downBtn = document.createElement("button");
+    downBtn.classList.add("downBtn");
     upBtn.innerText = "UP";
     downBtn.innerText = "DOWN";
-    const floorLabel = document.createElement("span");
-    floorLabel.textContent = `Floor ${j}`;
+    if (j == noOfFloors) {
+      upBtn.style.display = "none";
+    }
+    if (j == 1) {
+      downBtn.style.display = "none";
+    }
     upBtn.onclick = function () {
       moveLift(j);
     };
@@ -40,13 +56,13 @@ function handleSubmit() {
       moveLift(j);
     };
     const buttons = document.createElement("div");
-
+    const line = document.createElement("hr");
     buttons.appendChild(upBtn);
     buttons.appendChild(downBtn);
-    buttons.appendChild(floorLabel);
     floorcontrollers.appendChild(buttons);
     floorcontrollers.classList.add("floorcontrollers");
     floor.appendChild(floorcontrollers);
+    floor.appendChild(line);
     simulation.appendChild(floor);
   }
   const GetFloor = document.querySelectorAll(".floorcontrollers");
@@ -60,12 +76,37 @@ function moveLift(targetFloor) {
     (lift) => lift.getAttribute("isMoving") === "false"
   );
   console.log(availableLift);
-
-  availableLift.style.transform = `translateY(${-120 * (targetFloor - 1)}px)`;
-  availableLift.style.transitionDuration = `${2 * Math.abs(targetFloor)}s`;
+  const currentY =
+    parseFloat(availableLift.style.transform.match(/-?\d+\.?\d*/)) || 0;
+  const targetY = -126.6 * (targetFloor - 1);
+  const floorsToMove = Math.abs((targetY - currentY) / 126.6);
+  availableLift.style.transform = `translateY(${targetY}px)`;
+  availableLift.style.transitionDuration = `${floorsToMove * 2}s`;
   availableLift.setAttribute("isMoving", "true");
+  DoorAnimation(floorsToMove, availableLift);
+}
+
+function DoorAnimation(floorsToMove, availableLift) {
+  let doors = availableLift.querySelector(".doors");
+  let leftDoor = doors.querySelector(".leftDoor");
+  let rightDoor = doors.querySelector(".rightDoor");
 
   setTimeout(() => {
-    availableLift.setAttribute("isMoving", "false");
-  }, 1500 * targetFloor);
+    leftDoor.style.transform = `translateX(-50px)`;
+    rightDoor.style.transform = `translateX(50px)`;
+
+    leftDoor.style.transition = `all 2.5s ease-in-out`;
+    rightDoor.style.transition = `all 2.5s ease-in-out`;
+  }, 2500 * floorsToMove);
+
+  setTimeout(() => {
+    leftDoor.style.transform = `translateX(0px)`;
+    rightDoor.style.transform = `translateX(0px)`;
+
+    leftDoor.style.transition = `all 2.5s ease-in-out`;
+    rightDoor.style.transition = `all 2.5s ease-in-out`;
+    setTimeout(() => {
+      availableLift.setAttribute("isMoving", "false");
+    }, 1500);
+  }, 2500 * floorsToMove + 2500);
 }
